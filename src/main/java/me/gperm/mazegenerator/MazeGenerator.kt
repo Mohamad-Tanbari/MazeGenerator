b77818e35dd35dc5d8bf77a274a7b744b0287f51
Mohamad-Tanbari <124389317+Mohamad-Tanbari@users.noreply.github.com>
2023-04-27 18:14:51 -0400

    Added test command to launch code.

--- src/main/java/me/gperm/mazegenerator/MazeGenerator.kt
+++ src/main/java/me/gperm/mazegenerator/MazeGenerator.kt
@@ -1,11 +1,14 @@
 package me.gperm.mazegenerator
 
+import me.gperm.mazegenerator.commands.Test
 import org.bukkit.plugin.java.JavaPlugin
 
 class MazeGenerator : JavaPlugin() {
     override fun onEnable() {
         // Plugin startup logic
         logger.info("Hello, World!")
+
+        getCommand("test")?.setExecutor(Test())
     }
 
     override fun onDisable() {
